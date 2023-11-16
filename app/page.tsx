d703a6e1404fe545e91a4a004d25bf59bf1e13d0
vini <velkhaliliborsato@gmail.com>
2023-11-16 17:11:49 -0300

    paste copy a bit easier

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import A from "@/components/FreakyLetters/A/A"
+import B from "@/components/FreakyLetters/B/B"
+import Z from "@/components/FreakyLetters/Z/Letter"
 
 export default function Home() {
   return (
     <>
     <A/>
+    <B/>
+    <Z/>
     </>
   )
 }
