7c85db028a9d3781595b24c11ff2ce69e6fe5421
vini <velkhaliliborsato@gmail.com>
2023-11-16 16:26:38 -0300

    learned how to fix id messy references

--- components/FreakyLetters/A/A.tsx
+++ components/FreakyLetters/A/A.tsx
@@ -7,13 +7,9 @@ const A = () => {
     return (
         <div style={{display:"flex"}}>
 
-        <div style={{width:100,height:200,border:"1px solid white"}}>
-            <A1/>
-        </div>
         
-        <div style={{width:100,height:200,border:"1px solid white"}}>
-            <A2/>
-        </div>
+        <div style={{width:100,height:200,border:"1px solid white"}}><A1/></div>
+        <div style={{width:100,height:200,border:"1px solid white"}}><A2/></div>
         
         </div>
     );
