675ce7697513afba91e4a769c3ccbc5ac3af9784
vini <velkhaliliborsato@gmail.com>
2023-11-21 15:31:30 -0300

    opacity 0

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -1,24 +1,16 @@
 "use client"
 import Word from "@/components/Words/word"
 import style from "@/sass/Home.module.scss"
-import gsap from "gsap";
 import Hero from "./Hero";
+import TheText from "./TheText";
 
 export default function HomePage() {
     
     return (
     <div className={style.Home}>
       <div className={style.gridContainer}>
-        
-      <Hero/>
-    
-      <div className={style.freakyLetters}>
-          <Word word="The quick"/>
-          <Word word="brown fox"/>
-          <Word word="jumps over"/>
-          <Word word="a lazy dog"/>
-      </div>
-    
+        <Hero/>
+        <TheText/>
       </div>
     </div>
 
---
+++ components/HomePage/TheText.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import gsap from "gsap";
+import style from "@/sass/Home.module.scss"
+import Word from "@/components/Words/word"
+
+
+const TheText = () => {
+    
+    return (
+        <div className={style.freakyLetters}>
+          <Word word="The quick"/>
+          <Word word="brown fox"/>
+          <Word word="jumps over"/>
+          <Word word="a lazy dog"/>
+        </div>
+    );
+}
+ 
+export default TheText;
