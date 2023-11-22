569d6cbb8358ab5dcaedabc9894b1e8209ffc60e
vini <velkhaliliborsato@gmail.com>
2023-11-21 22:59:21 -0300

    nice letters

--- components/HomePage/Hero.tsx
+++ components/HomePage/Hero.tsx
@@ -17,31 +17,34 @@ const Hero = () => {
             .fromTo(".heroH1",{opacity:0,x:"-25%"},{opacity:1,x:0})
             .fromTo(".separator",{opacity:0,width:0},{opacity:1,width:"50%"})
             .fromTo(".paragraph",{opacity:0,x:"25%"},{opacity:1,x:0})
+            .fromTo(".freakyLetters1",{opacity:0,x:"25%"},{opacity:1,x:0})
+            .fromTo(".freakyLetters2",{opacity:0,x:"25%"},{opacity:1,x:0})
+            .fromTo(".freakyLetters3",{opacity:0,x:"25%"},{opacity:1,x:0})
 
         }, rootRef)
         return () => context.revert()
     },[])
 
     return (
-        <div className={style.Home}>
+        <div className={style.Home} ref={rootRef}>
           <div className={style.gridContainer}>
 
-            <div className={`${style.hero} hero`} ref={rootRef} >
+            <div className={`${style.hero} hero`}>
                 <h1 className="heroH1">
-                    <div>BAGULHOS</div>
-                    <div>BRISA</div>
+                    <div>Nice</div>
+                    <div>Letters</div>
                 </h1>
 
                 <div className={`${style.separator} separator`}/>
                 <p className="paragraph">
-                    Uns bagulho brisa pra ficar brisand. Feito em NextJS.
+                    look at it
                 </p>
             </div>
 
             <div className={`${style.freakyLetters} freakyLetters`}>
-                <Word word="gente"/>
-                <Word word="que"/>
-                <Word word="brisa"/>
+                <div className="freakyLetters1"><Word word="nice"/></div>
+                <div className="freakyLetters2"><Word word="freaking"/></div>
+                <div className="freakyLetters3"><Word word="letters"/></div>
             </div>
 
           </div>
