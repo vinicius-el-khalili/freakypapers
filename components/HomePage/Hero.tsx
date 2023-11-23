c9c0b6d65d49e7b71aad6e5384bdd217a8888a77
vini <velkhaliliborsato@gmail.com>
2023-11-23 18:57:40 -0300

    nice letters

--- components/HomePage/Hero.tsx
+++ components/HomePage/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
             timelineRef.current = gsap.timeline()
             .fromTo(".heroH1",{opacity:0,x:"-25%"},{opacity:1,x:0})
             .fromTo(".separator",{opacity:0,width:0},{opacity:1,width:"50%"})
-            .fromTo(".paragraph",{opacity:0,x:"25%"},{opacity:1,x:0})
+            .fromTo(".paragraph",{opacity:0,x:"25%"},{opacity:1,x:0, duration:1})
             .fromTo(".freakyLetters1",{opacity:0,x:"25%"},{opacity:1,x:0})
             .fromTo(".freakyLetters2",{opacity:0,x:"25%"},{opacity:1,x:0})
             .fromTo(".freakyLetters3",{opacity:0,x:"25%"},{opacity:1,x:0})
@@ -31,20 +31,20 @@ const Hero = () => {
 
             <div className={`${style.hero} hero`}>
                 <h1 className="heroH1">
-                    <div>Nice</div>
-                    <div>Letters</div>
+                    <div>freaky</div>
+                    <div>letters</div>
                 </h1>
 
                 <div className={`${style.separator} separator`}/>
                 <p className="paragraph">
-                    look at it
+                    a proof of concept implementation of something
                 </p>
             </div>
 
             <div className={`${style.freakyLetters} freakyLetters`}>
-                <div className="freakyLetters1"><Word word="nice"/></div>
-                <div className="freakyLetters2"><Word word="freaking"/></div>
-                <div className="freakyLetters3"><Word word="letters"/></div>
+                <div className="freakyLetters1"><Word word="abcdefgh"/></div>
+                <div className="freakyLetters2"><Word word="ijklmnopq"/></div>
+                <div className="freakyLetters3"><Word word="rstuvwyzx"/></div>
             </div>
 
           </div>
